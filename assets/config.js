b5e82874f520f2dd46e59ff2f8894abf94d202e0
Josi <123311017+Shorksi@users.noreply.github.com>
2024-05-01 02:34:18 +0200

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,32 @@
+let config = {
+  "Title": "Shorksi - Social Media",
+  "Name": "Shorksi",
+  "Description": "Hier findest du alle meine Social Media Channels :3",
+  "links": [
+    {
+      "Title": "Twitch",
+      "URL": "https://twitch.tv/shorksi",
+      "icon_classes": "fab fa-twitch"
+    },
+    {
+      "Title": "YouTube",
+      "URL": "https://youtube.com/NotJonnyYT",
+      "icon_classes": "fab fa-youtube"
+    },
+    {
+      "Title": "Twitter",
+      "URL": "https://twitter.com/Shorksi",
+      "icon_classes": "fas fa-twitter"
+    },
+    {
+      "Title": "Discord",
+      "URL": "https://discord.gg/4DvgajEVS3",
+      "icon_classes": "fab fa-discord"
+    },
+    {
+      "Title": "osu!",
+      "URL": "https://osu.ppy.sh/users/schlaflose",
+      "icon_classes": "fab fa-osu"
+    },
+  ]
+}
